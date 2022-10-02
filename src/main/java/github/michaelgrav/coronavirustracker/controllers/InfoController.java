c6425e5d60a9d956a2adf1c9f3c1cc4abc6a2551
Michael Graves <mgrav621@gmail.com>
2022-10-02 10:14:10 -0500

    Added separate pages for pie chart and application info

---
+++ src/main/java/github/michaelgrav/coronavirustracker/controllers/InfoController.java
@@ -0,0 +1,22 @@
+package github.michaelgrav.coronavirustracker.controllers;
+
+import github.michaelgrav.coronavirustracker.models.LocationStats;
+import github.michaelgrav.coronavirustracker.services.CoronavirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.NumberFormat;
+import java.util.*;
+
+@Controller
+public class InfoController {
+
+    @GetMapping("/info") // root url
+    public String info() {
+        return "info.html"; // Maps to the info html file
+    }
+}
